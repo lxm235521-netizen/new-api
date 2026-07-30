a40da24d3de5e7e62128d6e90bd7ab44b4bc452c
lxm235521 <lxm235521@gmail.com>
2026-07-30 15:19:47 +0800

    fix: restore missing service import in sora adaptor

--- relay/channel/task/sora/adaptor.go
+++ relay/channel/task/sora/adaptor.go
@@ -17,6 +17,7 @@ import (
 	"github.com/QuantumNous/new-api/relay/channel"
 	taskcommon "github.com/QuantumNous/new-api/relay/channel/task/taskcommon"
 	relaycommon "github.com/QuantumNous/new-api/relay/common"
+	"github.com/QuantumNous/new-api/service"
 	"github.com/QuantumNous/new-api/setting/system_setting"
 
 	"github.com/gin-gonic/gin"
